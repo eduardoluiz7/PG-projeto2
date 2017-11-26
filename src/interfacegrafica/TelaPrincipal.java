b9ebd4af30147b4bafa004e4e6e8b3f69cc7b68e
Eduardo Luiz Silva <els6@cin.ufpe.br>
2017-11-26 19:30:41 -0300

    Criando arquivos do Projeto

--- src/interfacegrafica/TelaPrincipal.java
+++ src/interfacegrafica/TelaPrincipal.java
@@ -6,7 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
+/**
+ * 
+ * @author eduardoluiz (els6)
+ *
+ */
 public class TelaPrincipal extends JFrame {
 
 	private JPanel contentPane;
